23caafd7aecac2b69e0a7ffa6b37820f92b4d82a
joel <jlmukono@gamil.com>
2019-03-03 19:27:42 +0300

    add function to add dice rolls values

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,41 @@
-
-
-var roll= Math.floor((Math.random() * 6) + 1);
-var totalScore =0 ;
-
-
-function replay(){
-if(roll==1){
-  alert("repeat");
+function playerInfo(score,status){
+  this.score=score;
+  this.status= status;
 }
-}
-replay();
 
-if(roll>1&& roll<6){
-  score=roll+=roll;
-  alert(score);
+
+
+playerInfo.prototype.rollDice= function(){
+  return Math.floor((Math.random() * 5) + 1);
 }
+
+ var playerOne = new playerInfo();
+ var playerTwo = new playerInfo();
+
+score = 0;
+playerOneTotalScoreArray = [];
+playerOneScoreArray = [];
+
+
+
+
+
+function checkOne(){
+  if(playerOneScore==1){
+
+    alert("you score nothing");
+
+  }
+}
+
+//checkOne();
+
+function totalRolls(){
+  for(var i=0;i<=150;i++){
+    playerOneScore = playerOne.rollDice();
+    playerOneTotalScoreArray.push(playerOneScore);
+    score=score+playerOneScore;
+    alert(score);
+  }
+}
+totalRolls();
